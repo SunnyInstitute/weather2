f6bd725567c00a2e85c2825f8232ff319158026a
Ajay-0101 <meenaajay9098855484@gmail.com>
2023-12-12 19:01:36 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import Products from "./componants/products";
-import {  createContext, useState } from "react";
-function App() {
-  var [cart,setCart]=useState([])
-  var MyContext=createContext()
-  return (
-    <>
-    <MyContext.Provider value={{city:"Bhopal"}}>
-      <Products/>
-    </MyContext.Provider>
-    </> 
-  )
+export default function App()
+{
+  return(<>
+  <h1>Hello world</h1>
+  </>)
 }
-
-export default {App,MyContext};
